8367fab65acf8fdb602bb123e3ab682be7e71eae
voldemort <alabenothman@gmail.com>
2021-04-06 10:57:54 -0700

    finished user model

--- musicapp/app/models/user.rb
+++ musicapp/app/models/user.rb
@@ -2,15 +2,17 @@ class User < ApplicationRecord
 validates :email, :session_token, presence: true, uniqueness: true 
 validates :password_digest, presence: true
 validates :password, length: {minimum: 6, allow_nil: true }
-    
-end
+
+    after_initialize :ensure_session_token 
+
+    attr_reader :password 
 
 
 def self.generate_session_token
      SecureRandom::urlsafe_base64
 end
 
-def self.reset_session_token! 
+def reset_session_token! 
     self.session_token = User.generate_session_token 
     self.save!   #can we say self.session_token.save??
     self.session_token
@@ -18,8 +20,32 @@ def self.reset_session_token!
 
 end
 
-def self.ensure_session_token 
+def ensure_session_token 
     self.session_token ||= User.generate_session_token
 end
 
+def password=(password) 
+    self.password_digest = Bcrypt::Password.create(password)
+    @password = password 
+end
 
+def is_password?(password) #to check the user's password when they login
+    password_obj = Bcrypt::Password.create(self.password_digest)
+    password_obj.is_password?(password)
+end
+
+  def User.find_by_credentials(email,password) 
+    #to find the specific user and check if the user exist and if the password is right .then we return the user else nil ( this method is made for the create section inside the controller )
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+        user 
+    else 
+        nil 
+    end
+
+
+  end
+
+
+
+end
